ec5de2770d02503a9091836b3b9960ece5292a05
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-12-17 00:09:55 +0000

    Added entries_2.csv and tests for reading it

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -47,8 +47,8 @@ RSpec.describe AddressBook do
         end
     end
     
-    #Test import from csv
-    describe "#import_from_csv" do
+    #Test import from entries.csv
+    describe "#import_from_entries.csv" do
         it "imports the correct number of entries" do
             book.import_from_csv("entries.csv")
             book_size = book.entries.size
@@ -87,6 +87,34 @@ RSpec.describe AddressBook do
             entry_five = book.entries[4]
             check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
         end
+    end
+    
+    describe "#import_from_entries_2.csv" do
+        it "imports the correct number of entries" do
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+            #NL: Checks the size of the entries in AddressBook (we know they are 3)
+            expect(book_size).to eq 3
+        end
+        
+        it "imports the 1st entry" do
+            book.import_from_csv("entries_2.csv")
+            entry_one = book.entries[0]
+            check_entry(entry_one, "Alberto", "55-2922-5657", "alberto@blocmail.com")
+        end
+        it "imports the 2nd entry" do
+            book.import_from_csv("entries_2.csv")
+            # Check the second entry
+            entry_two = book.entries[1]
+            check_entry(entry_two, "Marco", "55-3899-4741", "marco@blocmail.com") 
+        end
+        it "imports the 3rd entry" do
+            book.import_from_csv("entries_2.csv")
+            # Check the third entry
+            entry_three = book.entries[2]
+            check_entry(entry_three, "Paola", "55-2335-4377", "paola@blocmail.com")
+        end
+
     end
     
     #Test for removal of entries
